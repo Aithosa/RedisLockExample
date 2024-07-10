2bdc3475832a4394089febad21b73d9e3bf1610b
Aithusa <32020141+Aithosa@users.noreply.github.com>
2024-07-11 01:19:10 +0800

    init project

---
+++ src/main/java/com/example/redislock/RedisLockExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.redislock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLockExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLockExampleApplication.class, args);
+    }
+
+}
